ccbdb01083bae2b459cfd9a87443e50566ccf29c
llim1 <loglim11@gmail.com>
2025-11-27 11:00:57 -0500

    album browser using android_auto_helper

--- lib/services/carplay_helper.dart
+++ lib/services/carplay_helper.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:finamp/services/music_player_background_task.dart';
 import 'package:flutter_carplay/flutter_carplay.dart';
 import 'package:audio_service/audio_service.dart';
@@ -16,6 +19,7 @@ import 'finamp_settings_helper.dart';
 import 'finamp_user_helper.dart';
 import 'jellyfin_api_helper.dart';
 import 'queue_service.dart';
+import 'android_auto_helper.dart';
 
 class CarPlayHelper {
   // logger?
@@ -23,6 +27,7 @@ class CarPlayHelper {
   ConnectionStatusTypes connectionStatus = ConnectionStatusTypes.unknown;
   final FlutterCarplay _flutterCarplay = FlutterCarplay();
 
+  final _androidAutoHelper = GetIt.instance<AndroidAutoHelper>();
   final _finampUserHelper = GetIt.instance<FinampUserHelper>();
   final _jellyfinApiHelper = GetIt.instance<JellyfinApiHelper>();
   final _downloadsService = GetIt.instance<DownloadsService>();
@@ -38,12 +43,33 @@ class CarPlayHelper {
 
   void onConnectionChange(ConnectionStatusTypes status) {
     if (status == ConnectionStatusTypes.connected) {
-      FlutterCarplay.showSharedNowPlaying(animated: true);
     }
   }
 
-  void setCarplayRootTemplate() { 
-    FlutterCarplay.setRootTemplate(
+  Future<void> setCarplayRootTemplate() async { 
+    List<MediaItem> rootItems = await GetIt.instance<MusicPlayerBackgroundTask>().getChildren(AudioService.browsableRootId);
+    CPListSection librarySection = CPListSection(
+      items: []);
+
+    for (final item in rootItems) {
+      librarySection.items.add(CPListItem(
+        text: item.title,
+        onPress: (complete, self) {
+          final parentId = MediaItemId.fromJson(jsonDecode(item.id) as Map<String, dynamic>);
+
+          switch(parentId.contentType) {
+            case TabContentType.albums: showAlbumsTemplate(parentId);
+            case TabContentType.artists: showArtistsTemplate(item);
+            case TabContentType.playlists:
+            case TabContentType.genres:
+            case TabContentType.tracks: 
+          }
+          complete();
+        }
+      ));
+    } 
+
+    await FlutterCarplay.setRootTemplate(
       rootTemplate: CPTabBarTemplate(
         templates: [
           CPListTemplate(
@@ -75,19 +101,20 @@ class CarPlayHelper {
           ),
           CPListTemplate(
             sections: [
-              CPListSection(
-                items: [
-                  CPListItem(
-                    text: "Albums", 
-                    onPress: (complete, self) {
-                      complete();
-                    }),
-                  CPListItem(text: "Artists"),
-                  CPListItem(text: "Playlists"),
-                  CPListItem(text: "Genres"),
-                  CPListItem(text: "Tracks"),
-                ]
-              )
+              // CPListSection(
+              //   items: [
+              //     CPListItem(
+              //       text: "Albums", 
+              //       onPress: (complete, self) {
+              //         complete();
+              //       }),
+              //     CPListItem(text: "Artists"),
+              //     CPListItem(text: "Playlists"),
+              //     CPListItem(text: "Genres"),
+              //     CPListItem(text: "Tracks"),
+              //   ]
+              // ),
+            librarySection
             ],
             title: 'Library',
             emptyViewTitleVariants: ['Library'],
@@ -100,10 +127,75 @@ class CarPlayHelper {
       ),
     );
 
-    _flutterCarplay.forceUpdateRootTemplate();
+    await _flutterCarplay.forceUpdateRootTemplate();
   }
 
-  void show() { 
-    GetIt.instance<MusicPlayerBackgroundTask>().skipToNext();
+  Future<void> showPlaylistTemplate(MediaItemId parent) async { 
+    // List<MediaItem> mediaItems = await GetIt.instance<MusicPlayerBackgroundTask>().getChildren(parent.id);
+    List<MediaItem> mediaItems = await _androidAutoHelper.getMediaItems(parent);
+
+    CPListSection playlistSection = CPListSection(items: []);
+
+    // Add shuffle play to top 
+    playlistSection.items.add(CPListItem(
+      text: "Shuffle Play", 
+      onPress: (complete, self) async { 
+        await _androidAutoHelper.playFromMediaId(parent, order: FinampPlaybackOrder.shuffled);
+        complete();
+      },
+    ));
+
+    mediaItems.asMap().forEach((index, item) { 
+      final itemId = MediaItemId.fromJson(jsonDecode(item.id) as Map<String, dynamic>);
+
+      playlistSection.items.add(CPListItem(
+        text: item.title,
+        detailText: item.artist,
+        onPress: (complete, self) async {
+          await _androidAutoHelper.playFromMediaId(parent, index: index);
+          complete();
+        },
+        ));
+    });
+
+    CPListTemplate playlistTemplate = CPListTemplate(
+      sections: [
+        playlistSection,
+      ], 
+      systemIcon: 'gear');
+  
+    await FlutterCarplay.push(template: playlistTemplate);
   }
+
+  Future<void> showAlbumsTemplate(MediaItemId parent) async { 
+    // Fetch child items of root navigation 
+    List<MediaItem> mediaItems = await _androidAutoHelper.getMediaItems(parent);
+    // List<MediaItem> childItems = await _androidAutoHelper.getMediaItems(parentId);
+  
+    CPListSection albumsSection = CPListSection(items: []);
+
+    for (final item in mediaItems) { 
+      final itemId = MediaItemId.fromJson(jsonDecode(item.id) as Map<String, dynamic>);
+
+      albumsSection.items.add(CPListItem(
+        text: item.title,
+        detailText: item.artist,
+        onPress: (complete, self) async {
+          await showPlaylistTemplate(itemId);
+          complete();
+        }
+        ));
+    }
+
+    CPListTemplate albumsTemplate = CPListTemplate(
+      sections: [
+        albumsSection,
+      ], 
+      systemIcon: 'gear');
+  
+    await FlutterCarplay.push(template: albumsTemplate);
+  }
+}
+
+Future<void> showArtistsTemplate(MediaItem parent) async { 
 }
